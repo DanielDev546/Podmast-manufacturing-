9e69be863a8a89494b37c354632640328ec487d7
Daniel <amunddaniel9@gmail.com>
2026-01-16 21:15:52 +0100

    add new file contact.js

---
+++ contact.js
@@ -0,0 +1,13 @@
+function openModal(){
+    
+    let area = document.getElementById("space");
+    if (area.value !== ""){
+     
+        area.innerText = ('Your response has been submitted');
+    } 
+    else{
+        alert("You have to type a message first");
+    }
+} if(true) {
+
+}   
